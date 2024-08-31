c9bc4378bb5cba4e28e19b8acd01c200745480ab
AdamRiedel <Adam.Riedel@baywa.de>
2024-08-31 21:18:11 +0200

    Add the round and the point-system

--- index.js
+++ index.js
@@ -2,24 +2,34 @@ import {
   generateRandomColor,
   fillRoundColors,
   shuffle,
+  checkColors,
+  setGame,
 } from "./src/libs/tools.js";
 
 const hexcode = document.querySelector("#hexcode");
 const colorsContainer = document.querySelector(".colors-container");
 const colorboxes = document.querySelectorAll(".colorbox");
+const startGameBtn = document.querySelector(".toggle-btn");
+const spanRounds = document.querySelector(".rounds");
+const spanPoints = document.querySelector(".points");
+
 let searchedColor = null;
 let fillColors = [];
 let mixedArray = [];
-const startGameBtn = document.querySelector(".toggle-btn");
+let rounds = 0;
+let points = 0;
 
 startGameBtn.addEventListener("click", () => {
   searchedColor = generateRandomColor();
   fillColors = fillRoundColors(11);
   fillColors.push(searchedColor);
   mixedArray = shuffle(fillColors);
+  spanRounds.innerText = 1;
+  rounds = 1;
+
   setGame(searchedColor, mixedArray, colorboxes, hexcode);
 
-  colorsContainer.addEventListener("click", (event) => {
+  function colorBoxEvent(event) {
     if (event.target.className !== "colorbox") {
       return;
     }
@@ -28,8 +38,23 @@ startGameBtn.addEventListener("click", () => {
 
     if (!result) {
       console.log("Leider Falsch");
+      setGame("Start Game", [], colorboxes, hexcode);
+      colorsContainer.removeEventListener("click", colorBoxEvent);
+      // Scoreboard einfügen (Localstorage)
+      rounds = 0;
+      points = 0;
+      spanRounds.innerText = rounds;
+      spanPoints.innerText = points;
       return;
     }
+
+    console.log(spanRounds.innerText);
+    rounds += 1;
+    points += 5;
+    spanRounds.innerText = rounds;
+    spanPoints.innerText = points;
     console.log(result);
-  });
+  }
+
+  colorsContainer.addEventListener("click", colorBoxEvent);
 });
